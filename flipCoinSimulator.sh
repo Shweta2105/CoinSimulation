3286cf95052e5bbdd641f95a4c17c33ce7375443
Shweta2105 <sghadi21@gmail.com>
2021-05-16 23:53:53 +0530

    Added: Multple flips

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,27 @@
 #!/bin/bash -x
 
-headOrTail=$((RANDOM%2))
-if [ $headOrTail -eq 0 ]
+tails=0
+heads=0
+MAX_FLIP=5
+num_of_flip=0
+while [ $num_of_flip -le $MAX_FLIP ]
+do
+	((num_of_flip++))
+	headOrTail=$((RANDOM%2))
+	if [ $headOrTail -eq 0 ]
+	then
+		heads=$(($heads+1))
+		echo "head"
+	else
+		tails=$(($tails+1))
+		echo "Tail"
+	fi
+done
+
+if [ $heads -gt $tails ]
 then
-	echo "head"
+	echo "Head wins"
 else
-	echo "Tail"
+	echo "Tail wins"
 fi
 
